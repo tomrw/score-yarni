a50a2930be0f091c9565c4a503d583c731ead79d
Tom West <tomrichardwest@gmail.com>
2018-01-03 12:22:02 +1100

    Style the player component

--- src/components/new-game/game-setup/styles/player.js
+++ src/components/new-game/game-setup/styles/player.js
@@ -1,25 +1,34 @@
 import { Dimensions, StyleSheet } from 'react-native';
 
 const { width: windowWidth } = Dimensions.get('window');
-
+const sidePadding = 10;
+const containerWidth = windowWidth - (sidePadding * 2);
 const playerRemoveButtonWidth = 20;
 const playerRemoveNameMargin = 10;
+const removeButtonHeight = 30;
 
 export default StyleSheet.create({
 	container: {
-		flex: 1,
 		flexDirection: 'row',
-		paddingLeft: 10
+		paddingBottom: 5,
+		paddingLeft: sidePadding,
+		paddingRight: sidePadding,
+		width: containerWidth
 	},
 	playerName: {
-		paddingRight: 10,
-		width: windowWidth - playerRemoveButtonWidth - playerRemoveNameMargin
+		fontSize: 18,
+		width: containerWidth - playerRemoveButtonWidth - playerRemoveNameMargin
 	},
 	removePlayerContainer: {
 		backgroundColor: '#ccc',
+		borderRadius: 5,
+		height: 30,
+		marginLeft: playerRemoveNameMargin,
+		overflow: 'hidden',
 		width: playerRemoveButtonWidth
 	},
 	removePlayerText: {
+		lineHeight: removeButtonHeight,
 		textAlign: 'center'
 	}
 });
