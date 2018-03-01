97f818705973a650f5a300f0fe266d28116b55bc
Tom West <tomrichardwest@gmail.com>
2018-03-01 17:25:07 +0800

    Reset the overflow styling when creating new players
    I'll sort this out another day

--- src/components/new-game/styles/game-setup.js
+++ src/components/new-game/styles/game-setup.js
@@ -4,16 +4,5 @@ export default StyleSheet.create({
 	container: {
 		height: '100%'
 	},
-	closeButton: {
-		position: 'absolute',
-		right: 0,
-		top: 0
-	},
-	playerListWrapper: {
-		bottom: 35,
-		left: 0,
-		position: 'absolute',
-		right: 0,
-		top: 80
-	}
+	playerListWrapper: {}
 });
