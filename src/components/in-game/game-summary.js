dc4d2580c9cde6e27000ae5cb3119fe74ad120a8
Tom West <tomrichardwest@gmail.com>
2018-03-03 08:42:53 +0800

    Add game summary component

---
+++ src/components/in-game/game-summary.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text } from 'react-native-elements';
+import { View } from 'react-native';
+
+import Leaderboard from './leaderboard';
+import Scoreboard from './scoreboard';
+
+const GameSummary = ({ leaderboard, players, scores }) => {
+	const leaderboardData = getLeaderboardData(leaderboard, players);
+	const scoreboardData = getScoreboardData(players, scores);
+
+	return (
+		<View>
+			<Text h4>Leaderboard</Text>
+			<Leaderboard data={ leaderboardData } />
+			<Text h4>Scoreboard</Text>
+			<Scoreboard scoreboardData={ scoreboardData } />
+		</View>
+	);
+};
+
+const getLeaderboardData = (leaderboard, players) => {
+	const leaderboardData = leaderboard.map(({ id, position, score }) => {
+		const { name } = players.find(el => el.id === id);
+
+		return {
+			name,
+			position,
+			score
+		};
+	});
+
+	return leaderboardData;
+};
+
+const getScoreboardData = (players, scores) => {
+	return players.map(({ id, name }) => {
+		const playerScores = scores
+			.filter(score => score.id === id)
+			.map(score => score.score);
+
+		return {
+			name,
+			scores: playerScores
+		};
+	});
+};
+
+GameSummary.propTypes = {
+	leaderboard: PropTypes.arrayOf(PropTypes.shape({
+		id: PropTypes.number.isRequired,
+		score: PropTypes.number.isRequired
+	})).isRequired,
+	players: PropTypes.arrayOf(PropTypes.shape({
+		id: PropTypes.number.isRequired,
+		name: PropTypes.string.isRequired
+	})).isRequired,
+	scores: PropTypes.arrayOf(PropTypes.shape({
+		id: PropTypes.number.isRequired,
+		score: PropTypes.number.isRequired
+	})).isRequired
+};
+
+export default GameSummary;
---
+++ tests/components/in-game/game-summary.spec.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import GameSummary from '../../../src/components/in-game/game-summary';
+
+describe('Given <GameSummary />', () => {
+	const player1 = { name: 'Tom', id: 1 };
+	const player2 = { name: 'Fred', id: 2 };
+	const players = [ player1, player2 ];
+	const score1 = { id: player1.id, position: 1, score: 100 };
+	const score2 = { id: player2.id, position: 2, score: 10 };
+	const leaderboard = [ score1, score2 ];
+	const scores = [
+		{ id: 1, score: 10 },
+		{ id: 2, score: 20 },
+		{ id: 1, score: 30 },
+		{ id: 2, score: 40 }
+	];
+	const props = {
+		players,
+		leaderboard,
+		scores
+	};
+	const renderedComponent = shallow(<GameSummary { ...props } />);
+
+	it('should be a `View`', () => {
+		expect(renderedComponent.is('View')).toBe(true);
+	});
+
+	describe('and its first child', () => {
+		const leaderboardHeading = renderedComponent.childAt(0);
+
+		it('should be a `Text`', () => {
+			expect(leaderboardHeading.is('TextElement')).toBe(true);
+		});
+
+		it('should have a `h4` prop', () => {
+			expect(leaderboardHeading.prop('h4')).toBe(true);
+		});
+	});
+
+	describe('and its second child', () => {
+		const leaderboard = renderedComponent.childAt(1);
+
+		it('should be a `Leaderboard`', () => {
+			expect(leaderboard.is('Leaderboard')).toBe(true);
+		});
+
+		it('should have a `data` prop', () => {
+			const expectedData = [
+				{ name: player1.name, position: score1.position, score: score1.score },
+				{ name: player2.name, position: score2.position, score: score2.score }
+			];
+
+			expect(leaderboard.prop('data')).toEqual(expectedData);
+		});
+	});
+
+	describe('and its third child', () => {
+		const scoreboardHeading = renderedComponent.childAt(2);
+
+		it('should be a `Text`', () => {
+			expect(scoreboardHeading.is('TextElement')).toBe(true);
+		});
+
+		it('should have a `h4` prop', () => {
+			expect(scoreboardHeading.prop('h4')).toBe(true);
+		});
+	});
+
+	describe('and its fourth child', () => {
+		const scoreboard = renderedComponent.childAt(3);
+
+		it('should be a `Scoreboard`', () => {
+			expect(scoreboard.is('Scoreboard')).toBe(true);
+		});
+
+		it('should have a `scoreboardData` prop', () => {
+			const scores1 = scores.filter(score => score.id === player1.id).map(score => score.score);
+			const scores2 = scores.filter(score => score.id === player2.id).map(score => score.score);
+			const expectedData = [
+				{ name: player1.name, scores: scores1 },
+				{ name: player2.name, scores: scores2 }
+			];
+
+			expect(scoreboard.prop('scoreboardData')).toEqual(expectedData);
+		});
+	});
+});
