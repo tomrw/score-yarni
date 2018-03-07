c73311a9d620824df1be54dbd7a9efe66995b586
Tom West <tomrichardwest@gmail.com>
2018-03-07 19:17:04 +0800

    Add mock config option for storing past games

--- src/components/settings/setting-config.js
+++ src/components/settings/setting-config.js
@@ -3,5 +3,10 @@ export default [
 		text: 'Reverse Scoreboard',
 		description: 'Show the latest scores at the top',
 		key: 'reverseScoreboard'
+	},
+	{
+		text: 'Past games to store',
+		description: 'The number of previous games to keep record of',
+		key: 'previousGameStorageCount'
 	}
 ];
