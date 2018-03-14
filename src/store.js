6bb33d2640f30c67936625235d5cd35944d4ecf0
Tom West <tomrichardwest@gmail.com>
2018-03-14 11:11:40 +0800

    Add navigation middleware when setting up store

--- src/store.js
+++ src/store.js
@@ -6,13 +6,14 @@ import { persistStore, persistReducer } from 'redux-persist';
 
 import initialState from './initial-state';
 import reducers from './reducers';
+import { navigationMiddleware } from './navigation/redux';
 
 const persistConfig = {
 	key: 'root',
 	storage,
 	whitelist: [ 'settings' ]
 };
-const middlewares = [ thunk, logger ];
+const middlewares = [ thunk, logger, navigationMiddleware ];
 const middleware = applyMiddleware(...middlewares);
 const persistedReducer = persistReducer(persistConfig, reducers);
 
