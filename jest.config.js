4ca38651489d8b922cd5f506172eff64bedead3b
Tom West <tomrichardwest@gmail.com>
2018-03-14 11:04:02 +0800

    Ignore react native and navigation in jest tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
 	collectCoverage: true,
 	coverageDirectory: 'coverage',
 	preset: 'react-native',
-	setupTestFrameworkScriptFile: './tests/setup.js'
+	setupTestFrameworkScriptFile: './tests/setup.js',
+	'transformIgnorePatterns': [
+		'node_modules/(?!(jest-)?react-native|react-navigation)'
+	]
 };
