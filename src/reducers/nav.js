129da7a44a42e000b88c1144e4d43990e02f7fe4
Tom West <tomrichardwest@gmail.com>
2018-03-24 16:37:00 +0800

    Return to the home screen when pressing back when in game

--- src/reducers/nav.js
+++ src/reducers/nav.js
@@ -1,9 +1,20 @@
+import { NavigationActions } from 'react-navigation';
+
 import AppNavigator from '../navigation/app';
 
 const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('HOME'));
 
 export default (state = initialState, action) => {
-	const nextState = AppNavigator.router.getStateForAction(action, state);
+	let nextState = AppNavigator.router.getStateForAction(action, state);
+
+	if (state && action.type === NavigationActions.BACK) {
+		if (state.routes[state.index].routeName === 'GAME_IN_PROGRESS') {
+			nextState = AppNavigator.router.getStateForAction(
+				NavigationActions.popToTop(),
+				state
+			);
+		}
+	}
 
 	return nextState || state;
 };
