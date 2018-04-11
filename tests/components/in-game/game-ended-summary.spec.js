eee4eeb68023b86efd3810afc2534b183227c798
Tom West <tomrichardwest@gmail.com>
2018-04-11 15:16:53 +0700

    Display the winners in the game ended summary

--- tests/components/in-game/game-ended-summary.spec.js
+++ tests/components/in-game/game-ended-summary.spec.js
@@ -7,7 +7,8 @@ import GameEndedSummary from '../../../src/components/in-game/game-ended-summary
 import gameEndedSummaryStyles from '../../../src/components/in-game/styles/game-ended-summary';
 
 describe('Given <GameEndedSummary />', () => {
-	const renderedComponent = shallow(<GameEndedSummary />);
+	const winners = [ 'Tom' ];
+	const renderedComponent = shallow(<GameEndedSummary winners={ winners } />);
 
 	it('should be a `View`', () => {
 		expect(renderedComponent.is(View)).toBe(true);
@@ -27,11 +28,44 @@ describe('Given <GameEndedSummary />', () => {
 		it('should have the `text` styles', () => {
 			expect(text.prop('style')).toEqual(gameEndedSummaryStyles.text);
 		});
+	});
 
-		it('should have the expected text', () => {
-			const expectedText = 'This game has ended!';
+	describe('when there is only ONE winner', () => {
+		const winners = [ 'Tom' ];
+		const renderedComponent = shallow(<GameEndedSummary winners={ winners } />);
+		const text = renderedComponent.childAt(0);
+		const textContent = text.props().children;
 
-			expect(text.props().children).toEqual(expectedText);
+		it('should read correctly', () => {
+			const expectedText = 'Tom wins!';
+
+			expect(textContent).toEqual(expectedText);
+		});
+	});
+
+	describe('when there are TWO winners', () => {
+		const winners = [ 'Tom', 'Fred' ];
+		const renderedComponent = shallow(<GameEndedSummary winners={ winners } />);
+		const text = renderedComponent.childAt(0);
+		const textContent = text.props().children;
+
+		it('should read correctly', () => {
+			const expectedText = 'Tom & Fred win!';
+
+			expect(textContent).toEqual(expectedText);
+		});
+	});
+
+	describe('when there are more than TWO winners', () => {
+		const winners = [ 'Tom', 'Fred', 'Chloe' ];
+		const renderedComponent = shallow(<GameEndedSummary winners={ winners } />);
+		const text = renderedComponent.childAt(0);
+		const textContent = text.props().children;
+
+		it('should read correctly', () => {
+			const expectedText = 'Tom, Fred & Chloe win!';
+
+			expect(textContent).toEqual(expectedText);
 		});
 	});
 });
