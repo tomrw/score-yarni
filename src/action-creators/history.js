6d9a4c0d411972cbeb717558cf2907d8ec87e41e
Tom West <tomrichardwest@gmail.com>
2018-04-22 18:03:24 +0700

    Add remove history entry action

--- src/action-creators/history.js
+++ src/action-creators/history.js
@@ -1,8 +1,15 @@
-import { MOVE_GAME_TO_HISTORY } from '../constants/history';
+import { MOVE_GAME_TO_HISTORY, REMOVE_HISTORY_ENTRY } from '../constants/history';
 
 export const addGameToHistory = game => ({
 	type: MOVE_GAME_TO_HISTORY,
 	payload: {
 		game
+	}
+});
+
+export const removeGameFromHistory = id => ({
+	type: REMOVE_HISTORY_ENTRY,
+	payload: {
+		id
 	}
 });
--- tests/action-creators/history.spec.js
+++ tests/action-creators/history.spec.js
@@ -1,4 +1,4 @@
-import { addGameToHistory } from '../../src/action-creators/history';
+import { addGameToHistory, removeGameFromHistory } from '../../src/action-creators/history';
 
 describe('Given the `history` action creator', () => {
 	describe('when adding a game to history', () => {
@@ -11,6 +11,19 @@ describe('Given the `history` action creator', () => {
 
 		it('should return the correct payload', () => {
 			expect(action.payload).toEqual({ game });
+		});
+	});
+
+	describe('when removing a game from history', () => {
+		const id = 10;
+		const action = removeGameFromHistory(id);
+
+		it('should return the correct type', () => {
+			expect(action.type).toEqual('REMOVE_HISTORY_ENTRY');
+		});
+
+		it('should return the correct payload', () => {
+			expect(action.payload).toEqual({ id });
 		});
 	});
 });
