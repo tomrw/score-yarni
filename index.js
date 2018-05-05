e25f3c92021f12919e133c392fe28bb461d5ae00
Tom West <tomrichardwest@gmail.com>
2018-05-05 20:23:24 +0700

    Rename ios package name to yarni

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/index';
 
-AppRegistry.registerComponent('cardGameScorerNative', () => App);
+AppRegistry.registerComponent('yarni', () => App);
